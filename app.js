43567d80d82ee8578a5e40fb33fc3e736642780a
Manoj Kumar <95513071+Iamsidar07@users.noreply.github.com>
2023-05-31 17:01:18 +0530

    Optimized transcript and summary routes

--- app.js
+++ app.js
@@ -1,30 +1,38 @@
 import express from "express";
 import Cors from "cors";
-import ytVideoTranscript from "./routes/ytVideoTranscriptRoutes.js";
-import ytVideoSummariz from "./routes/summarizeRoutes.js";
+import youtubeVideoTranscript from "./src/routes/youtubeVideoTranscriptRoutes.js";
+import youtubeVideoSummary from "./src/routes/youtubeVideoSummaryRoutes.js";
 
 const app = express();
 
 const port = process.env.port || 8080;
 
-app.use(Cors())
-app.use(express.json({ limit: "50mb" }))
+// Enable Cross-Origin Resource Sharing (CORS)
+app.use(Cors());
 
-app.use("/api/v1/transcript", ytVideoTranscript);
-app.use("/api/v1/summariz", ytVideoSummariz);
+// Parse JSON requests with a limit of 50mb
+app.use(express.json({ limit: "50mb" }));
 
+// Route middleware for handling /api/v1/transcript requests
+app.use("/api/v1/transcript", youtubeVideoTranscript);
+
+// Route middleware for handling /api/v1/summary requests
+app.use("/api/v1/summary", youtubeVideoSummary);
+
+// Function to start the server
 const startServer = () => {
     try {
+        // Route handler for the root endpoint
         app.get("/", async (req, res) => {
-            res.send("Hello world from YtVideoTranscript");
-        })
+            res.send("Hello world from YoutubeVideoTranscript");
+        });
+
+        // Start the server and listen on the specified port
         app.listen(port, () => console.log(`Server is running on port ${port}`));
     } catch (error) {
-        console.error(error)
+        console.error(error);
     }
-}
+};
 
+// Call the startServer function to start the server
 startServer();
-
-// Example usage:
-// const videoId = '1Wq5itfNNnE';
